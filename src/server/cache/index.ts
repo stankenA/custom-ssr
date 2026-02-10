55c34e0246aecf597d4f92906d45e4cf30faac36
Artem Stankevich <artem.stankevich@redcollar.ru>
2026-02-10 13:32:11 +0300

    fix: fix configs

--- src/server/cache/index.ts
+++ src/server/cache/index.ts
@@ -6,6 +6,7 @@ type CacheItem = {
 const cache = new Map<string, CacheItem>();
 
 export const getCache = (key: string) => {
+  console.log(cache);
   return cache.get(key);
 };
 
--- src/server/render/render-page.tsx
+++ src/server/render/render-page.tsx
@@ -4,27 +4,12 @@ import { Html } from "../../shared/html-template";
 
 export const renderPage = (
   app: ReactElement,
-  options?: {
-    jsSrc?: string;
-    initialData?: unknown;
-  },
 ) => {
   const appHtml = renderToString(app);
 
   const html = renderToString(
     <Html>
       <div id='root' dangerouslySetInnerHTML={{ __html: appHtml }} />
-      {options?.initialData ? (
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `window.__INITIAL_DATA__ = ${JSON.stringify(
-              options.initialData,
-            )}`,
-          }}
-        />
-      ) : null}
-
-      {options?.jsSrc ? <script type='module' src={options.jsSrc} /> : null}
     </Html>,
   );
 
